039cd2aa814a613f8899c3b563e5db4d3a18e64d
imtirtha <tirthake@gmail.com>
2025-02-23 22:37:05 +0600

    Add files via upload
    display  first 10 natural numbers

---
+++ display 10 natural number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    printf("Write a program in C to display the first 10 natural numbers.\n");
+
+    int i;
+    for (i>0; i<=10; i++)
+    {
+        printf("%d\t",i);
+
+    }
+    return 0;
+}
