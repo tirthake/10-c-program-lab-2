6bd8dc5a71591c2b227cc9a346d07bc4b650a9eb
imtirtha <tirthake@gmail.com>
2025-02-23 22:45:48 +0600

    Update display 10 natural number.c

--- display 10 natural number.c
+++ display 10 natural number.c
@@ -4,7 +4,7 @@ int main()
     printf("Write a program in C to display the first 10 natural numbers.\n");
 
     int i;
-    for (i>0; i<=10; i++)
+    for (i=1; i<=10; i++)
     {
         printf("%d\t",i);
 
