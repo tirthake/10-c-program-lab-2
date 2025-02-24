466194ede7063f383ec3af77485d77a0c2ced726
imtirtha <tirthake@gmail.com>
2025-02-24 23:27:54 +0600

    Update leap year.c

--- leap year.c
+++ leap year.c
@@ -8,7 +8,7 @@ int main()
 
     if(year%400==0)
         printf("leap year");
-    else if (year%4==0 && year%100!=0)
+    else if (year%4==0 && year%100!=0) // that mean if year is modulas by 4 =0 and % by 100 is not equal to 0 is a leap year.
         printf("leap year");
     else
         printf("not leap year");
