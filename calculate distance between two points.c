f96da0de8280e5bc33459cc4e0027a44e2d0c523
imtirtha <tirthake@gmail.com>
2025-02-28 18:30:44 +0600

    Update calculate distance between two points.c

--- calculate distance between two points.c
+++ calculate distance between two points.c
@@ -7,13 +7,19 @@ int main()
     printf("Enter the value of x1:");
     scanf("%d",&x1);
 
-    printf("Enter the value of y1:");
-    scanf("%d",&y1);
+        printf("Enter the value of y1:");
+         scanf("%d",&y1);
+    
     printf("Enter the value of x2:");
     scanf("%d",&x2);
-    printf("Enter the value of y2:");
-    scanf("%d",&y2);
+    
+         printf("Enter the value of y2:");
+          scanf("%d",&y2);
+
+
+    
     distance= sqrt(pow(x2-x1,2)+pow(y2-y1,2)); // use pow for power
+    
     printf("\n The distance between two points are:%.4f",distance);// use .4 for 4 digit fraction
     return 0;
 }
