b39eae9f200f31b43a5a16500737c5f1803c3ccc
imtirtha <tirthake@gmail.com>
2025-02-28 18:38:14 +0600

    Update compute your weight to the moon s weights.c
    calculate your earth weight  in moon . we know that The moon's gravity is approximately 1/6th (or about 16.6%) of Earth's gravity.

--- compute your weight to the moon s weights.c
+++ compute your weight to the moon s weights.c
@@ -2,19 +2,13 @@
 int main()
 {
     float eweight,mweight;
-    printf("enter your weight on earth:");
+    printf("enter your weight on earth:(KG)");
     scanf("%f",&eweight);
-    //moon gravity is 17 parcebt than earth
+    //moon gravity is 17 parcent than earth
     //we know weight=g*m
 
 
-    mweight =eweight*0.17;
+    mweight =eweight*0.17;// moon weight = earth weight * 1/6
     printf("your weight in moon is :%f\n",mweight);
-
-
-
-
-
-
     return 0;
 }
